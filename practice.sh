e5eb0dfaa003a0912b2994e87ac96e2238bb37b5
rahul singh <rahul2018me015abesit@gmail.com>
2026-08-28 08:31:33 +0530

    updated practice.sh

--- practice.sh
+++ practice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "this is a feature branch"
+echo "this is a feature -x branch"
 echo "hello this is a practice file"
 echo "new line updated"
 echo "demonstratio of git conflict"
